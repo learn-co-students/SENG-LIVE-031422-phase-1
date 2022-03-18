e8416acf9e5dda2101c07d71c432d32a979114e3
Aysan Isayo <aisayo@icloud.com>
2022-03-18 13:22:04 -0700

    solution

--- 05-dom-manipulation/index.js
+++ 05-dom-manipulation/index.js
@@ -1,4 +1,4 @@
-const pokemons = [
+const pokemon = [
   {
     id: 1,
     name: "bulbasaur",
@@ -30,3 +30,148 @@ const pokemons = [
     likes: 11,
   },
 ];
+
+// arrays/iteration
+
+// what methods do we have available to use?
+// for loops, for of, .forEach, .map, .find, .filter
+
+// What am I trying to achieve? Which method is best for this goal?
+
+// I want to pass each character inside the pokemon to another function? Which method is best for this task and why?
+
+// .map function : if i was changing the element, this is the tool i want to use
+// for...of: using this tool we would have to write another function that we can pass the element to
+// .forEach: don't need a return value
+
+// let newArr = []
+// function loopPokemon(arr) {
+//   for (const el of arr) {
+//     // pass el to another function
+//     newArr.push(el)
+//     anotherFun(el);
+//   }
+// }
+
+// function anotherFun(el) {
+//   console.log(el);
+// }
+
+// arr.forEach((el) => console.log(el));
+
+// arr.forEach(function (el) {
+//   console.log(el);
+// });
+
+// // how does map work: it breaks apart our array, dealing with each individual element independently
+// // what is being passed to the callback function inside the (): each element in the array
+// // what is the return value of the result once map has completed? a new array with the changed elements
+// const newArr = arr.map((element) => {
+//   // we can do something with the element here
+//   // running some task on each element by itself
+// })
+
+// we use map if we want to modify elements
+
+// objects
+
+// dealing with objects
+
+// 1. how to create an object
+// a create a new character with name, img, likes as the properties/keys
+
+// const newChar = {
+//   name: "pikachu"
+// }
+
+// // 2. first understand what the object looks like, then use the properties to return the values 
+
+// // return pikachu 
+// newChar.name // dot notation is used more commonly, especially when we know the literal property name 
+// newChar["name"] // bracket notation is used when we use variables
+
+// What is the DOM? 
+// Document Object Model
+// A representation of our HTML 
+// Something we are going to interact with to Create, Read, Update and Delete [CRUD]
+
+// READ: how do we select elements on the DOM
+
+// using selectors:
+// id: no element should share the same id on the DOM, notation: #
+// class: notation: dot
+// tag name
+
+// document.getElementById();
+// will accept the elements ID as an argument 
+// used the elements id property to select and return 
+// Returns a single element, the first match
+// restriction: it can only be called on the document object 
+
+const pokeForm = document.getElementById('poke-form')
+
+// document.getElementsByClassname 
+// returns a list of elements, returns all the elements with the same class name 
+// return a HTMLcollection
+// Can convert to an array using the Array.from()
+
+const labels = document.getElementsByClassName('form-label')
+
+// document.querySelector()
+// not limited to just Id, we can pass in class names, ids and tags 
+// returns one element
+// need to include the selector notation in the argument i.e #id, .class
+// we can call these on descendants of document
+
+const lectureDiv = document.querySelector('#lecture-goals')
+
+// document.querySelectorAll()
+// can be used for tags and classes... if you're using it for ID's re-evaluate
+// still returns a list: NodeList, forEach works on a nodelist 
+
+const allDivs = document.querySelectorAll('div')
+
+
+// create elements 
+
+// select my target for all the character cards 
+
+const pokeContainer = document.querySelector('#poke-container')
+
+
+// method used to create a new element is: document.createElement(element)
+
+
+// i have an array of pokemon characters: pokemon 
+// I need to generate a card for each character inside this array  
+
+pokemon.forEach((character) => renderPokemon(character))
+
+
+// a function responsible for creating each character called renderPokemon()
+
+function renderPokemon(char){
+console.log(char)
+  // create the card is a div  pokeCard 
+
+  const pokeCard = document.createElement('div')
+  pokeCard.id = `poke-${char.id}`
+  pokeCard.className = 'poke-card'
+
+  // append  
+  // appendChild
+  
+  // inside the pokeCard, need to create an img element
+  const pokeImg = document.createElement('img')
+  pokeImg.src = char.img
+  pokeImg.alt = `${char.name} image`
+
+  // add the img to the DOM but where???
+  pokeCard.append(pokeImg)
+  
+  // inside the pokeCard, need to create h3 tag with the characters name  
+  
+  // 
+  // slap the pokeCard on the DOM
+  pokeContainer.appendChild(pokeCard)
+}
